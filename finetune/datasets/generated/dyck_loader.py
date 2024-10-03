006f40fa96d6776a993f06f2042ecf53c7eccefb
Sid <sid.data.universe@gmail.com>
2024-10-03 11:26:26 -0700

    Generate an even distribution of reference answer lengths.

--- finetune/datasets/generated/dyck_loader.py
+++ finetune/datasets/generated/dyck_loader.py
@@ -26,45 +26,68 @@ DYCK_CHARACTER_PAIRS = [("<", ">"), ("[", "]"), ("{", "}"), ("(", ")")]
 DYCK_CHALLENGE_PROMPT = "Complete the rest of the sequence, making sure that the parentheses are closed properly. Input: "
 
 
+def count_contiguous_matching_suffix_characters(
+    word: str, characters: typing.Set[str]
+) -> int:
+    """Count the number of contiguous characters from the end of the word that are in the specified set.
+
+    Args:
+        word (str): Word to check for number of continous matching suffix characters.
+        characters (typing.Set[str]):Characters to match on.
+
+    Returns:
+        int: Number of characters from the end of the word that all match the characters specified.
+    """
+    char_count = 0
+    for char in reversed(word):
+        if char in characters:
+            char_count += 1
+        else:
+            break
+    return char_count
+
+
+def generate_dyck(
+    dyck_character_pairs: typing.List[typing.Tuple[str, str]],
+    pair_count: int,
+) -> str:
+    """Generate a Dyck word (a balanced string of brackets).
+
+    Args:
+        dyck_character_pairs (int): Pairs of open/close characters to use.
+        pair_count (int): Number of pairs to generate when creating the Dyck word.
+
+    Returns:
+        str: A randomly generated Dyck word.
+    """
+    open_count = 0
+    close_count = 0
+    unused_close_characters_stack = []
+    dyck_word = ""
+
+    # Generate the word.
+    for _ in range(pair_count * 2):
+        # Randomly choose to open or close unless we have nothing open or already have every pair opened.
+        if open_count - close_count == 0 or (
+            open_count < pair_count and random.choice([True, False])
+        ):
+            # Pick a random character pair
+            dyck_char_pair = random.choice(dyck_character_pairs)
+            # Add the open character to the word now.
+            dyck_word += dyck_char_pair[0]
+            open_count += 1
+            # Add the close character to the to be used close character list
+            unused_close_characters_stack.append(dyck_char_pair[1])
+        else:
+            # Pop off the stack
+            close_char = unused_close_characters_stack.pop()
+            dyck_word += close_char
+            close_count += 1
+
+    return dyck_word
+
+
 class DyckLoader:
-    def _generate_dyck(
-        self, dyck_character_pairs: typing.List[typing.Tuple[str, str]], pair_count: int
-    ) -> str:
-        """Generate a Dyck word (a balanced string of brackets).
-
-        Args:
-            dyck_character_pairs (int): Pairs of open/close characters to use.
-            pair_count (int): Number of pairs to generate when creating the Dyck word.
-
-        Returns:
-            str: A randomly generated Dyck word.
-        """
-        open_count = 0
-        close_count = 0
-        unused_close_characters_stack = []
-        dyck_word = ""
-
-        # Generate the word.
-        for _ in range(pair_count * 2):
-            # Randomly choose to open or close unless we have nothing open or already have every pair opened.
-            if open_count - close_count == 0 or (
-                open_count < pair_count and random.choice([True, False])
-            ):
-                # Pick a random character pair
-                dyck_char_pair = random.choice(dyck_character_pairs)
-                # Add the open character to the word now.
-                dyck_word += dyck_char_pair[0]
-                open_count += 1
-                # Add the close character to the to be used close character list
-                unused_close_characters_stack.append(dyck_char_pair[1])
-            else:
-                # Pop off the stack
-                close_char = unused_close_characters_stack.pop()
-                dyck_word += close_char
-                close_count += 1
-
-        return dyck_word
-
     def __init__(
         self,
         dyck_character_pairs: typing.List[
@@ -88,36 +111,51 @@ class DyckLoader:
             random_seed (typing.Optional[int], optional): Seed to use for all random operations if set.
             samples (int, optional): Number of samples to generate. Defaults to 100.
         """
+        if min_length_answer > max_length_answer:
+            raise ValueError(
+                "min_length_answer: {min_length_answer} is greater than max_length_answer {max_length_answer}."
+            )
+
+        if min_length_pairs > max_length_pairs:
+            raise ValueError(
+                "min_length_pairs: {min_length_pairs} is greater than max_length_pairs {max_length_pairs}."
+            )
+
+        if max_length_answer > max_length_pairs:
+            raise ValueError(
+                "max_length_answer: {max_length_answer} is greater than max_length_pairs {max_length_pairs}."
+            )
+
         self.buffer: typing.List[typing.Tuple[str, str]] = []
 
         if random_seed is not None:
             random.seed(random_seed)
 
         for _ in range(samples):
-            pair_count = random.randint(min_length_pairs, max_length_pairs)
-            complete_dyck = self._generate_dyck(dyck_character_pairs, pair_count)
+            # Decide the length the reference should be.
+            reference_length = random.randint(min_length_answer, max_length_answer)
+            # We need at least as many pairs as the reference length or the min length.
+            adjusted_min_length_pairs = max(reference_length, min_length_pairs)
+            # Decide total number of pairs.
+            pair_count = random.randint(adjusted_min_length_pairs, max_length_pairs)
 
-            # We will attempt to make the reference length this long depending on closing characters at the end.
-            attempted_reference_length = random.randint(
-                min_length_answer, max_length_answer
-            )
-            actual_reference_length = 0
+            # Generate a complete dyck that ends with at least reference_length close characters.
+            # Note that this is not optimized but is very fast even going up to 20 ending close characters.
+            complete_dyck = ""
+            dyck_suffix_close_chars = 0
             close_chars = {pair[1] for pair in DYCK_CHARACTER_PAIRS}
-            for char in reversed(complete_dyck):
-                if (
-                    actual_reference_length < attempted_reference_length
-                    and char in close_chars
-                ):
-                    actual_reference_length += 1
-                else:
-                    break
+            while dyck_suffix_close_chars < reference_length:
+                complete_dyck = generate_dyck(dyck_character_pairs, pair_count)
+                dyck_suffix_close_chars = count_contiguous_matching_suffix_characters(
+                    complete_dyck, close_chars
+                )
 
             # Generate the reference answer by taking the last reference length characters.
-            reference = " ".join(complete_dyck[-actual_reference_length:])
+            reference = " ".join(complete_dyck[-reference_length:])
 
             # Generate the challenge by prepending the prompt and removing the reference characters.
             challenge = DYCK_CHALLENGE_PROMPT + " ".join(
-                complete_dyck[:-actual_reference_length]
+                complete_dyck[:-reference_length]
             )
 
             self.buffer.append((challenge, reference))
