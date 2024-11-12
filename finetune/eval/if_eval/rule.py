8276e18c81b480edcb4d086f3d05b2af227e07f2
Sid <sid.data.universe@gmail.com>
2024-11-11 18:32:02 -0800

    Add additional planned rules.

--- finetune/eval/if_eval/rule.py
+++ finetune/eval/if_eval/rule.py
@@ -13,6 +13,39 @@ class RuleId(IntEnum):
     # Word count must be at least a specified threshold.
     WORD_COUNT_AT_LEAST = 2
 
+    # Sentence count must be at most a specified threshold.
+    SENTENCE_COUNT_AT_MOST = 3
+
+    # Sentence count must be at least a specified threshold.
+    SENTENCE_COUNT_AT_LEAST = 4
+
+    # All letters in the output are uppercase.
+    ALL_UPPER_CASE = 5
+
+    # All letters in the output are lowercase.
+    ALL_LOWER_CASE = 6
+
+    # Not commas in the output.
+    NO_COMMAS = 7
+
+    # The specified word(s) appear at least once.
+    KEYWORD_INCLUSION = 8
+
+    # The specified word appears a specific number of times.
+    KEYWORD_FREQUENCY = 9
+
+    # The specified word(s) do not appear.
+    KEYWORD_FORBIDDEN = 10
+
+    # There must be a specific number of bullet points in the output.
+    BULLET_COUNT_FREQUENCY = 11
+
+    # The output must start with the specified content.
+    STARTS_WITH = 12
+
+    # The output must end with the specified content.
+    ENDS_WITH = 13
+
 
 class IFEvalRule(ABC):
     """Base class for all IFEval rules."""
