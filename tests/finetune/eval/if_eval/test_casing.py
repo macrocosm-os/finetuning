75d6285753d32e5e9830876f521b6d8c71ba6d8a
Sid <sid.data.universe@gmail.com>
2024-11-16 08:52:12 -0800

    Also test casing of numbers works correctly.

--- tests/finetune/eval/if_eval/test_casing.py
+++ tests/finetune/eval/if_eval/test_casing.py
@@ -5,13 +5,13 @@ from finetune.eval.if_eval.casing import UppercaseRule, LowercaseRule
 class TestCasing(unittest.TestCase):
     def test_uppercase(self):
         rule = UppercaseRule()
-        self.assertTrue(rule.matches("THIS  IS A\n TEST, ALL UPPERCASE!", 0))
-        self.assertFalse(rule.matches("THIS HAS A LOWERCASE LETTEr.", 0))
+        self.assertTrue(rule.matches("THIS  IS A\n TEST 1, ALL UPPERCASE!", 0))
+        self.assertFalse(rule.matches("THIS 1 HAS A LOWERCASE LETTEr.", 0))
 
     def test_lowercase(self):
         rule = LowercaseRule()
-        self.assertTrue(rule.matches("this  is a\n test, all lowercase.", 0))
-        self.assertFalse(rule.matches("This has an uppercase letter.", 0))
+        self.assertTrue(rule.matches("this  is a\n test 1, all lowercase.", 0))
+        self.assertFalse(rule.matches("This 1 has an uppercase letter.", 0))
 
     def test_get_prompt_uppercase(self):
         rule = UppercaseRule()
