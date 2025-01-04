3fba5eedea8885f2fa11bac3322aa869efb8d6e4
rusticluftig <rusticluftig@gmail.com>
2025-01-04 13:12:26 -0800

    Bump version

--- constants/__init__.py
+++ constants/__init__.py
@@ -33,7 +33,7 @@ from finetune.eval.if_eval.version import IfEvalVersion
 # Project Constants.
 # ---------------------------------
 
-__version__ = "2.7.2"
+__version__ = "2.8.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
